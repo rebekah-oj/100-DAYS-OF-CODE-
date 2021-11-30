34d025d09440dd9af0b21c86252a9b3153868bcc
rebekah-oj <olojadeborah37@gmail.com>
2021-11-30 13:05:57 +0100

    Day Eight: Functions

---
+++ src/functions.py
@@ -0,0 +1,14 @@
+"""def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+
+greet_with_name("Jack Bauer")"""
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"what is it like in {location}")
+
+
+greet_with("Jack Meyers", "Lagos")
