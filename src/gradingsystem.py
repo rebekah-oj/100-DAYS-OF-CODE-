3c266403d0a5d2a3a7659ad801af400ef641eaf6
rebekah-oj <olojadeborah37@gmail.com>
2021-12-20 13:42:38 +0100

    Day Eleven: Nesting Dictionaries

---
+++ src/gradingsystem.py
@@ -0,0 +1,34 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# Scores 91 - 100: Grade = "Outstanding"
+#
+# Scores 81 - 90: Grade = "Exceeds Expectations"
+#
+# Scores 71 - 80: Grade = "Acceptable"
+#
+# Scores 70 or lower: Grade = "Fail"
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif 80 < score < 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif 70 < score < 80:
+        student_grades[student] = "Äcceptable"
+    elif score > 70:
+        student_grades[student] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
---
+++ src/nesting.py
@@ -0,0 +1,20 @@
+# Nesting Dictionary in a dictionary
+travel_log = {
+    "France": {"cities_visited": ['Paris', 'Lille', 'Dijon'], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5}
+}
+
+# Nesting Dictionary in a list
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ['Paris', 'Lille', 'Dijon'],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+]
